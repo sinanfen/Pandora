befc9e9c418559cd5f5979e83b04964332bbeb73
Sinan Fen <sinanfen@hotmail.com>
2024-09-18 01:39:35 +0300

    AutoMapper configurations

---
+++ Pandora.Application/AutoMapper/Mappings/UserProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Pandora.Application.DTOs.UserDTOs;
+using Pandora.Core.Domain.Constants.Enums;
+using Pandora.Core.Domain.Entities;
+
+namespace Pandora.Application.AutoMapper.Mappings;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<UserRegisterDto, User>()
+            .Include<UserRegisterDto, IndividualUser>()
+            .Include<UserRegisterDto, CorporateUser>();
+
+        CreateMap<UserRegisterDto, IndividualUser>()
+            .ForMember(dest => dest.UserType, opt => opt.MapFrom(src => UserType.Individual));
+
+        CreateMap<UserRegisterDto, CorporateUser>()
+            .ForMember(dest => dest.UserType, opt => opt.MapFrom(src => UserType.Corporate));
+
+        // Diğer mapping işlemleri
+        CreateMap<User, UserDto>().ReverseMap();
+        CreateMap<UserUpdateDto, User>().ReverseMap();
+    }
+}
