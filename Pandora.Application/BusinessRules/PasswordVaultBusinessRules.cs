f1784d6e9ece4cd77de0430b0e3bd389404c2217
Sinan Fen <sinanfen@hotmail.com>
2024-09-30 01:29:32 +0300

    business rules configured

--- Pandora.Application/BusinessRules/PasswordVaultBusinessRules.cs
+++ Pandora.Application/BusinessRules/PasswordVaultBusinessRules.cs
@@ -1,13 +1,66 @@
 ﻿using Pandora.Application.Interfaces.Repositories;
-namespace Pandora.Application.BusinessRules;
+using Pandora.Application.Security.Interfaces;
+using Pandora.CrossCuttingConcerns.ExceptionHandling;
+using System.Security.Authentication;
 
 public class PasswordVaultBusinessRules
 {
     private readonly IPasswordVaultRepository _passwordVaultRepository;
+    private readonly IHasher _hasher;
 
-    public PasswordVaultBusinessRules(IPasswordVaultRepository passwordVaultRepository)
+    public PasswordVaultBusinessRules(IPasswordVaultRepository passwordVaultRepository, IHasher hasher)
     {
         _passwordVaultRepository = passwordVaultRepository;
-    }   
-}
+        _hasher = hasher;
+    }
 
+    public void EnsurePasswordMeetsComplexityRules(string password)
+    {
+        if (password.Length < 8 || !password.Any(char.IsDigit) || !password.Any(char.IsUpper))
+        {
+            throw new BusinessException("Şifre en az 8 karakter uzunluğunda, bir büyük harf ve bir rakam içermelidir.");
+        }
+    }
+
+    public void EnsurePasswordChangeIsRecent(DateTime? lastPasswordChangeDate)
+    {
+        if (lastPasswordChangeDate.HasValue && (DateTime.UtcNow - lastPasswordChangeDate.Value).Days > 90)
+        {
+            throw new BusinessException("Bu şifre 90 günden fazla bir süredir değiştirilmedi.");
+        }
+    }
+
+    public void EnsurePasswordNotExpired(DateTime? passwordExpirationDate)
+    {
+        if (passwordExpirationDate.HasValue && passwordExpirationDate.Value < DateTime.UtcNow)
+        {
+            throw new BusinessException("Bu şifrenin geçerlilik süresi dolmuş.");
+        }
+    }
+
+    public async Task EnsurePasswordHashIsUnique(string passwordHash)
+    {
+        var exists = await _passwordVaultRepository.AnyAsync(pv => pv.PasswordHash == passwordHash);
+        if (exists)
+        {
+            throw new BusinessException("Bu şifre daha önce kullanılmış.");
+        }
+    }
+
+    public async Task CheckCurrentPasswordAsync(Guid passwordVaultId, string currentPassword)
+    {
+        var passwordVault = await _passwordVaultRepository.GetAsync(pv => pv.Id == passwordVaultId);
+
+        if (passwordVault == null)
+        {
+            throw new BusinessException("Şifre kasası bulunamadı.");
+        }
+
+        var isValidPassword = _hasher.VerifyHashedPassword(passwordVault.PasswordHash, currentPassword, HashAlgorithmType.Sha512);
+
+        if (!isValidPassword)
+        {
+            throw new BusinessException("Mevcut şifre doğru değil.");
+        }
+    }
+}
