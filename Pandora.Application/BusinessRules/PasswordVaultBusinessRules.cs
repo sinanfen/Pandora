79bb2d81cb9121ce983b3e45dec8fa21073c01f7
Sinan Fen <sinanfen@hotmail.com>
2024-09-29 03:58:47 +0300

    business rules created

---
+++ Pandora.Application/BusinessRules/PasswordVaultBusinessRules.cs
@@ -0,0 +1,13 @@
+﻿using Pandora.Application.Interfaces.Repositories;
+namespace Pandora.Application.BusinessRules;
+
+public class PasswordVaultBusinessRules
+{
+    private readonly IPasswordVaultRepository _passwordVaultRepository;
+
+    public PasswordVaultBusinessRules(IPasswordVaultRepository passwordVaultRepository)
+    {
+        _passwordVaultRepository = passwordVaultRepository;
+    }   
+}
+
---
+++ Pandora.Application/BusinessRules/PersonalVaultBusinessRules.cs
@@ -0,0 +1,14 @@
+﻿using Pandora.Application.Interfaces.Repositories;
+
+namespace Pandora.Application.BusinessRules;
+
+public class PersonalVaultBusinessRules
+{
+    private readonly IPersonalVaultRepository _personalVaultRepository;
+
+    public PersonalVaultBusinessRules(IPersonalVaultRepository personalVaultRepository)
+    {
+        _personalVaultRepository = personalVaultRepository;
+    }
+}
+
