e37aef15bbba19099f257245cda1c6dc88f7d53e
Sinan Fen <sinanfen@hotmail.com>
2024-09-29 03:58:40 +0300

    mappings created

---
+++ Pandora.Application/AutoMapper/Mappings/PasswordVaultProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Pandora.Application.DTOs.PasswordVaultDTOs;
+using Pandora.Core.Domain.Entities;
+
+namespace Pandora.Application.AutoMapper.Mappings;
+
+public class PasswordVaultProfile : Profile
+{
+    public PasswordVaultProfile()
+    {
+        CreateMap<PasswordVaultAddDto, PasswordVault>();
+        CreateMap<PasswordVaultDto, PasswordVault>();
+        CreateMap<PasswordVault, PasswordVaultDto>().ReverseMap();
+    }
+}
---
+++ Pandora.Application/AutoMapper/Mappings/PersonalVaultProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Pandora.Application.DTOs.PersonalVaultDTOs;
+using Pandora.Core.Domain.Entities;
+
+namespace Pandora.Application.AutoMapper.Mappings;
+
+public class PersonalVaultProfile : Profile
+{
+    public PersonalVaultProfile()
+    {
+        CreateMap<PersonalVaultAddDto, PersonalVault>();
+        CreateMap<PersonalVaultDto, PersonalVault>();
+        CreateMap<PersonalVault, PersonalVaultDto>().ReverseMap();
+    }
+}
